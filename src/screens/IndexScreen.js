e44f985ac2a2dfa1799b2e5c68731d4d576be58b
Ricardo <ricardo.vejarano.garcia@gmail.com>
2020-05-05 21:20:07 -0500

    improvements

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -31,9 +31,10 @@ const IndexScreen = ({ navigation }) => {
 
 IndexScreen.navigationOptions = ({ navigation }) => {
     return {
-        headerRight: <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+        headerRight: () => { return <TouchableOpacity onPress={() => navigation.navigate('Create')}>
             <Feather name="plus" size={30} />
         </TouchableOpacity>
+        }
     };
 };
 
