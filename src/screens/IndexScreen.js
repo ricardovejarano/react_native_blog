0a113f43f6ca60a912238daa3ddc549be23ef888
Ricardo <ricardo.vejarano.garcia@gmail.com>
2020-05-05 20:59:22 -0500

    Create post view

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'reac
 import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
-const IndexScreen = () => {
+const IndexScreen = ({ navigation }) => {
     const { state, addBlogPost, deleteBlogPost } = useContext(Context);
     return (
         <View>
@@ -15,18 +15,30 @@ const IndexScreen = () => {
                 data={state}
                 keyExtractor={blogPost => blogPost.title}
                 renderItem={({ item }) => {
-                    return <View style={styles.row}>
-                        <Text style={styles.title}>{item.title} - {item.id}</Text>
-                        <TouchableOpacity onPress={() => {
-                           deleteBlogPost(item.id)
-                        }}>
-                            <Feather style={styles.icon} name="trash" />
+                    return (
+                        <TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}>
+                            <View style={styles.row}>
+                                <Text style={styles.title}>{item.title} - {item.id}</Text>
+                                <TouchableOpacity onPress={() => {
+                                    deleteBlogPost(item.id)
+                                }}>
+                                    <Feather style={styles.icon} name="trash" />
+                                </TouchableOpacity>
+                            </View>
                         </TouchableOpacity>
-                    </View>
+                    );
                 }}
             />
         </View>
     );
+};
+
+IndexScreen.navigationOptions = ({ navigation }) => {
+    return {
+        headerRight: <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+            <Feather name="plus" size={30} />
+        </TouchableOpacity>
+    };
 };
 
 const styles = StyleSheet.create({
